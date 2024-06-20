3269d0b0e916c750bb10962f1bc529d108c8b9c1
qoen6uas <167756210+qoen6uas@users.noreply.github.com>
2024-06-20 21:34:14 +0900

    Form2.cs

--- abctest/Form2.cs
+++ abctest/Form2.cs
@@ -21,8 +21,8 @@ namespace abctest
     {
         IFirebaseConfig config = new FirebaseConfig
         {
-            AuthSecret = "0zEMJT0ibw3Adjj8O6ox9GpUrUZByncftWDyu9Wa",
-            BasePath = "https://fir-kangtaehun-default-rtdb.firebaseio.com/"
+            AuthSecret = "",
+            BasePath = ""
         };
 
         List<PRO> Pro = new List<PRO>();
